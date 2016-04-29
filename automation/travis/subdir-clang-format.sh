4735fa02515a7f395b596735bacc263a6e2613c8
Franziskus Kiefer <franziskuskiefer@gmail.com>
2016-04-29 14:45:30 +0200

    Update subdir-clang-format.sh

--- automation/travis/subdir-clang-format.sh
+++ automation/travis/subdir-clang-format.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 STATUS=0
-for i in $(find . -type f -name '*.[ch]' -print); do
+for i in $(find $1 -type f -name '*.[ch]' -print); do
   if ! clang-format-3.8 $i | diff $i - > tmpDiff; then
     echo "Sorry, $i is not formatted properly. Please use clang-format 3.8 on your patch before landing."
     cat tmpDiff
